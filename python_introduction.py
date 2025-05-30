f4c39aa37be09254103c7ddd71a2fdcf05900915
DIEGO POSADA <dapiposada@gmail.com>
2025-05-29 23:23:58 -0600

    modified:   python_basic.py 	modified:   python_introduction.py

---
+++ python_introduction.py
@@ -0,0 +1,34 @@
+#1) What is python 
+#python is a simple open-sourse, mmulti-paradigm and object-oriented, scripting programming language.
+
+#2) Python features 
+# -Hight-level 
+# -simple sintax
+# -third-party libraries 
+# -Multi-paradigm 
+# -Funcional
+# -Imperative
+# -Object-oriented
+# -Structured 
+# -Reflective
+
+#2.1) Python application fields
+#Artificial inteligence 
+#Data science 
+#Software programing 
+#Web developer 
+#Automation scriping 
+#Web crawler
+#Cloud computing 
+
+# But low performance compare to c & c++
+
+#3) How to use python 
+#IDE stands for integred development enviroment, it can increase programmer's prodictovity  by interating common programmon activities int a single sofrtware/
+
+#We can edit sourcem, run the program and debug using an IDE.
+
+# -PyCharm 
+# -Juupyter notebook 
+# -VS code 
+# -Spyder
